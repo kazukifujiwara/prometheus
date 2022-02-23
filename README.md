da1f17cbac858d9c314396011e8fc97133e4da1f
Kazuki Fujiwara <kazuki@Kazuki-MacBook-Pro.local>
2022-02-24 00:18:47 +0900

    added some prometheus_client modules

--- README.md
+++ README.md
@@ -3,7 +3,23 @@
 * ひとまず自宅のルータを監視できるようにした。
 * 監視対象のノードはprometheus.yamlに記載しているが、コンテナを停止せずに監視対象を追加することはできない・・・？（確認中）
 
+
+## 用語
+
+* exposition(開示): Prometheusがメトリクスをリイ用できる状態にするプロセスのこと
+    * Prometheusに対するメトリクスの開示はHTTPを介して行われる。
+
+## メトリクス名称の慣習
+
+* メトリクス名はスネークケースで記載する。
+* Counter, Summary, Histogram のメトリクスでは、_total, _count, _sum, _bucket というサフィックスを使う。
+    * Counterで常に、_totalをつけることを除けばこれらのサフィックスはつけないようにするべき。
+* 単位を一つに統一し、メトリクス名に含めるようにする。
+    * 秒単位のカウンタには、mymetric_seconds_total など
+
 ## 参考
 
 * 書籍：O'REILLY 入門 Prometheus
 * [Blackbox Exporter で死活監視](https://qiita.com/Yohichi_Hayashi/items/05d5c9f45862958b9499)
+* [Monitor Your Network With gNMI, SNMP, and Grafana](https://blog.networktocode.com/post/monitor_your_network_with_gnmi_snmp_and_grafana/)
+* [InfluxDB+Telegraf+Grafanaによるネットワークモニタリング](https://www.janog.gr.jp/meeting/janog40/application/files/2815/0154/9553/janog40-SP3NM-tetsusat-01.pdf)
--- httpserver/app.py
+++ httpserver/app.py
@@ -1,24 +1,100 @@
 import http.server
 import random
+import time
+import unittest
 
-from prometheus_client import start_http_server, Counter
+from prometheus_client import start_http_server, Counter, Gauge, Summary, Histogram, REGISTRY
 
-REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
-EXCEPTIONS = Counter('hello_worlds_exception_total', 'Exceptions serving Hello World.')
-SALES = Counter('hello_worlds_sales_euro_total', 'Euros made serving Hello World.')
+'''
+3.2章まで
+'''
+
+# REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
+# EXCEPTIONS = Counter('hello_worlds_exception_total', 'Exceptions serving Hello World.')
+# SALES = Counter('hello_worlds_sales_euro_total', 'Euros made serving Hello World.')
+
+# class MyHandler(http.server.BaseHTTPRequestHandler):
+#     @EXCEPTIONS.count_exceptions()
+#     def do_GET(self):
+#         REQUESTS.inc()
+#         euros = random.random()
+#         SALES.inc(euros)
+#         self.send_response(200)
+#         self.end_headers()
+#         self.wfile.write(f"Hello World for {euros} euros.".encode())
+        
+# if __name__ == "__main__":
+#     start_http_server(8000)
+#     server = http.server.HTTPServer(('', 8001), MyHandler)
+#     server.serve_forever()
+    
+'''
+3.3.1 prometheus_client.Gauge
+'''
+
+# # Gaugeのカウンタメトリクスの末尾には_totalをつけないのが慣習
+# INPROGRESS = Gauge('hello_worlds_inprogress', 'Number of Hello Worlds in progress.')
+# LAST = Gauge('hello_worlds_last_time_seconds', 'The last time a Hello World was served.')
+
+# class MyHandler(http.server.BaseHTTPRequestHandler):
+#     @INPROGRESS.track_inprogress()
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.end_headers()
+#         self.wfile.write(f"Hello World".encode())
+#         LAST.set(time.time())
+        
+# if __name__ == "__main__":
+#     start_http_server(8000)
+#     server = http.server.HTTPServer(('', 8001), MyHandler)
+#     server.serve_forever()
+    
+'''
+3.4 prometheus_client.Summary
+3.5 prometheus_client.Histogram
+'''
+
+# # LATENCY = Summary('hello_world_latency_seconds', 'Time for a request Hello World.')
+# LATENCY = Histogram('hello_world_latency_seconds',
+#                     'Time for a request Hello World.',
+#                     buckets=[0.0001, 0.0002, 0.0005, 0.001, 0.01, 0.1])
+
+# class MyHandler(http.server.BaseHTTPRequestHandler):
+#     @LATENCY.time()
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.end_headers()
+#         self.wfile.write(f"Hello World".encode())
+        
+# if __name__ == "__main__":
+#     start_http_server(8000)
+#     server = http.server.HTTPServer(('', 8001), MyHandler)
+#     server.serve_forever()
+    
+'''
+3.6 Instrumentation(測定) Test
+'''
+
+FOOS = Counter('foos_total', 'The number of foo calls.')
+
+def foo():
+    FOOS.inc()
+    
+class TestFoo(unittest.TestCase):
+    def test_counter_inc(self):
+        before = REGISTRY.get_sample_value('foos_total')
+        foo()
+        after = REGISTRY.get_sample_value('foos_total')
+        self.assertEqual(1, after-before)
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-    @EXCEPTIONS.count_exceptions()
+    @LATENCY.time()
     def do_GET(self):
-        REQUESTS.inc()
-        euros = random.random()
-        SALES.inc(euros)
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(f"Hello World for {euros} euros.".encode())
+        self.wfile.write(f"Hello World".encode())
         
 if __name__ == "__main__":
     start_http_server(8000)
     server = http.server.HTTPServer(('', 8001), MyHandler)
     server.serve_forever()
-    
